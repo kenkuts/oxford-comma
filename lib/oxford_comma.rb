da88cb6cd6b3710da95f93f1e767ea90ab874f17
kenkuts <kennethchoi19@gmail.com>
2020-03-15 07:00:22 -0500

    lmao

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,23 @@
 def oxford_comma(array)
+    string = ""
+
+    if array.size == 1
+        string << array[0]
+    elsif array.size == 2
+        string << "#{array[0]} and #{array[1]}" 
+    else
+        array.each_with_index do |thing, index|
+            if index == (array.size - 1)
+                string << thing
+            
+            elsif index != (array.size - 2)
+                string << "#{thing}, "
+            else
+                string << "#{thing}, and " 
+            end
+        end
+    end
+
+    string
 
 end
