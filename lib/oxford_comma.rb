f11796e17723ea6db5911686a7abec6faa8cc363
kenkuts <kennethchoi19@gmail.com>
2020-03-15 07:06:49 -0500

    refactored code using the insert method

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,23 +1,29 @@
 def oxford_comma(array)
-    string = ""
+    # string = ""
 
-    if array.size == 1
-        string << array[0]
-    elsif array.size == 2
-        string << "#{array[0]} and #{array[1]}" 
-    else
-        array.each_with_index do |thing, index|
-            if index == (array.size - 1)
-                string << thing
+    # if array.size == 1
+    #     string << array[0]
+    # elsif array.size == 2
+    #     string << "#{array[0]} and #{array[1]}" 
+    # else
+    #     array.each_with_index do |thing, index|
+    #         if index == (array.size - 1)
+    #             string << thing
             
-            elsif index != (array.size - 2)
-                string << "#{thing}, "
-            else
-                string << "#{thing}, and " 
-            end
-        end
+    #         elsif index != (array.size - 2)
+    #             string << "#{thing}, "
+    #         else
+    #             string << "#{thing}, and " 
+    #         end
+    #     end
+    # end
+    # string
+
+    if array.size == 2
+        return "#{array[0]} and #{array[1]}"
+    elsif 2 < array.size
+        array[-1].insert(0, "and ")
     end
 
-    string
-
+    array.join(", ")
 end
